f56d8791cbf3c9b9e6bda953af7250ee4e153e5c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-12-19 08:24:40 +0900

    Add threshold

--- public/components/App.js
+++ public/components/App.js
@@ -11,8 +11,9 @@ export default class App extends React.Component {
         } else {
           change.target.classList.remove('is-active');
         }
+        change.target.textContent = `${change.intersectionRatio * 100}%`;
       });
-    });
+    }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] });
     const steps = document.querySelectorAll('.step');
     steps.forEach((step) => {
       observer.observe(step);
@@ -23,9 +24,9 @@ export default class App extends React.Component {
     return (
       <div id="container">
         {[...Array(5)].map((_, i) => {
-          const v = 100 + Math.floor(Math.random() * window.innerHeight / 4);
-          const style = { padding: `${v}px 0px` };
-          return <div className="step" style={style}><p>STEP {i + 1}</p></div>;
+          // const v = 100 + Math.floor(Math.random() * window.innerHeight / 4);
+          // const style = { padding: `v}px 0px` };
+          return <div className="step"><p>STEP {i + 1}</p></div>;
         })}
       </div>
     );
